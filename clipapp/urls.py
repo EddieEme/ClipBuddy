0ccc5242c5f784918b759a354a4ba11ba6f026d7
Emeremnu Edison <edisonemeremnu@gmail.com>
2024-12-23 00:41:21 +0100

    added the loging page

--- clipapp/urls.py
+++ clipapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from clipapp import views
 
+app_name = 'clippapp'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login')
 ]
--- clipapp/views.py
+++ clipapp/views.py
@@ -5,3 +5,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'clipapp/index.html')
+
+def home(request):
+    return render(request, 'clipapp/home.html')
+
+
+def login_view(request):
+    return render(request, 'clipapp/login.html')
