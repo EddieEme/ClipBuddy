160587535ca374a3a2f4fc22be218d649ea6803b
Emeremnu Edison <edisonemeremnu@gmail.com>
2025-01-03 20:11:33 +0100

    fixed code

--- clipapp/models.py
+++ clipapp/models.py
@@ -17,6 +17,7 @@ class Snippet(models.Model):
 class UserTestimonial(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return f"{self.text} - {self.user.name}"
